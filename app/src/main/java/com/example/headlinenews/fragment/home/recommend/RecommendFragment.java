755bf53bc8f6dfa85866edc87461591deefbe76c
zhaoyanbo <3096481245@qq.com>
2021-06-18 08:48:46 +0800

    新闻项目

---
+++ app/src/main/java/com/example/headlinenews/fragment/home/recommend/RecommendFragment.java
@@ -0,0 +1,24 @@
+package com.example.headlinenews.fragment.home.history;
+
+import com.example.headlinenews.R;
+import com.example.headlinenews.base.BaseFragment;
+
+
+public class HistoryFragment extends BaseFragment {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int bindLayoutId() {
+        return R.layout.fragment_mine;
+    }
+}
